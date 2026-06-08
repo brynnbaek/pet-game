95993d14ee6c765dcb5d944d6302d7738814e624
brynnbaek <brynnbaek0125@gmail.com>
2026-06-08 06:32:13 +0000

    finished

---
+++ script.js
@@ -0,0 +1,10 @@
+water.onclick = () => {
+    <script>
+let step = 0;
+12ff12
+const dough = document.getElementById("dough");
+
+const jar = document.getElementById("jar");
+const flour = document.getElementById("flour");
+const water = document.getElementById("water");
+const mix = document.getElementById("mix");
